19f4220f7d1b7b097053ec114eec8d03715960b9
steve <steve.nguyen@linksinternational.com>
2023-12-22 15:29:39 +0800

    Add swagger

--- app.js
+++ app.js
@@ -19,11 +19,46 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.set('view engine', 'ejs')
 app.set("views", path.join(__dirname, "/templates"));
 
-app.get('/', (req, res) => {
+app.get('/index', (req, res) => {
   res.render('index')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
